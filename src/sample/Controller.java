7a7e900ee91e70d6ed28935b577f0f73d6570702
Tahsin Rashad <tahsin.rashad716@gmail.com>
2016-12-03 19:18:07 +0600

    connected Controller to UI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML
+    private Text display;
+
+    @FXML
+    private void onNumberClick(ActionEvent event) {
+
+        System.out.println("onNumberClick worked.");
+
+    }
+
+
+    @FXML
+    private void onOperatorClick(ActionEvent event) {
+
+        System.out.println("onOperatorClick worked.");
+    }
+
+
+
 }

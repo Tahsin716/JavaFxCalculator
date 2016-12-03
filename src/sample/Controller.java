f30f3999c1baa02c1e796ef7c594a0f8e1e85121
Tahsin Rashad <tahsin.rashad716@gmail.com>
2016-12-03 20:03:51 +0600

    Added Model

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,17 +2,30 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
-    private Text display;
+    private TextField display;
+
+    private String tempValue = "";
+    private Long tempNum = 0L;
+    private Boolean start = true;
+    private String operator = "";
 
     @FXML
     private void onNumberClick(ActionEvent event) {
 
-        System.out.println("onNumberClick worked.");
+        //System.out.println("onNumberClick worked.");
+        if(start) {
+            display.setText("");
+            start = false;
+        }
+
+        tempValue = ((Button)event.getSource()).getText();
+        display.setText(display.getText() + tempValue);
 
     }
 
@@ -20,7 +33,25 @@ public class Controller {
     @FXML
     private void onOperatorClick(ActionEvent event) {
 
-        System.out.println("onOperatorClick worked.");
+        //System.out.println("onOperatorClick worked.");
+        tempValue = ((Button)event.getSource()).getText();
+
+        if(!tempValue.equals("=")) {
+            if(!operator.isEmpty())
+                return;
+
+            operator = tempValue;
+            tempNum = Long.parseLong(display.getText());
+            display.setText("");
+        }
+        else {
+            if(operator.isEmpty())
+                return;
+
+            //display.setText(String.valueOf());
+            operator = "";
+            start = true;
+        }
     }
 
 

0af96636b2cce57654ee0fd3fa0e4d7d3040a1d5
Tahsin Rashad <tahsin.rashad716@gmail.com>
2016-12-03 20:09:22 +0600

    Mode calculateLong updated

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -2,4 +2,25 @@ package sample;
 
 public class Model {
 
+    public long calculateLong(long number1, long number2, String operator) {
+
+        if (operator.equals("+")) {
+            return number1 + number2;
+        }
+        else if (operator.equals("-")) {
+            return number1 - number2;
+        }
+        else if (operator.equals("*")) {
+            return number1 * number2;
+        }
+        else if (operator.equals("/")) {
+            if (number2 == 0)
+                return 0;
+
+            return number1 / number2;
+        }
+
+        System.out.println("Unknown operator - " + operator);
+        return 0;
+    }
 }

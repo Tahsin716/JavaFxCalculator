f4d1498fac2b2a4a8482b0b6a938fa85e463698f
Tahsin Rashad <tahsin.rashad716@gmail.com>
2016-12-14 19:31:15 +0600

    Exception Handling added

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -4,73 +4,92 @@ public class Model {
 
     public String dataProcess(String number1, String number2, String operator) {
 
-        String result;
+        String result = "0";
 
-        if(number1.contains(".") || number2.contains("."))
-            result = calculateDouble(Double.parseDouble(number1), Double.parseDouble(number2), operator);
-        else
-            result = calculateLong(Long.parseLong(number1), Long.parseLong(number2), operator);
+        try {
+
+            if(number1.contains(".") || number2.contains("."))
+                result = calculateDouble(Double.parseDouble(number1), Double.parseDouble(number2), operator);
+            else
+                result = calculateLong(Long.parseLong(number1), Long.parseLong(number2), operator);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return result;
     }
 
 
-    public String calculateLong(long number1, long number2, String operator) {
+    public String calculateLong(long number1, long number2, String operator){
 
-        if (operator.equals("+")) {
-            return String.valueOf(number1 + number2);
-        }
-        else if (operator.equals("-")) {
-            return String.valueOf(number1 - number2);
-        }
-        else if (operator.equals("x")) {
-            return String.valueOf(number1 * number2);
-        }
-        else if (operator.equals("/")) {
-            if (number2 == 0)
-                return String.valueOf(0);
+        try {
+            if (operator.equals("+")) {
+                return String.valueOf(number1 + number2);
+            }
+            else if (operator.equals("-")) {
+                return String.valueOf(number1 - number2);
+            }
+            else if (operator.equals("x")) {
+                return String.valueOf(number1 * number2);
+            }
+            else if (operator.equals("/")) {
+                if (number2 == 0)
+                    return String.valueOf(0);
 
-            if(number1 % number2 != 0)
-                return String.valueOf((double)number1 / (double)number2);
+                if(number1 % number2 != 0)
+                    return String.valueOf((double)number1 / (double)number2);
 
-            return String.valueOf(number1 / number2);
+                return String.valueOf(number1 / number2);
+            }
+
+            System.out.println("Unknown operator - " + operator);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
 
-        System.out.println("Unknown operator - " + operator);
         return String.valueOf(0);
     }
 
     public String calculateDouble(double number1, double number2, String operator) {
 
-        if (operator.equals("+")) {
-            if(String.valueOf(number1 + number2).endsWith(".0"))
-                return String.valueOf((long)(number1 + number2));
+       try {
 
-            return String.valueOf(number1 + number2);
-        }
-        else if (operator.equals("-")) {
-            if (String.valueOf(number1 - number2).endsWith(".0"))
-                return String.valueOf((long)(number1 - number2));
+           if (operator.equals("+")) {
+               if(String.valueOf(number1 + number2).endsWith(".0"))
+                   return String.valueOf((long)(number1 + number2));
 
-            return String.valueOf(number1 - number2);
-        }
-        else if (operator.equals("x")) {
-            if (String.valueOf(number1 * number2).endsWith(".0"))
-                return String.valueOf((long)(number1 * number2));
+               return String.valueOf(number1 + number2);
+           }
+           else if (operator.equals("-")) {
+               if (String.valueOf(number1 - number2).endsWith(".0"))
+                   return String.valueOf((long)(number1 - number2));
 
-            return String.valueOf(number1 * number2);
-        }
-        else if (operator.equals("/")) {
-            if (number2 == 0)
-                return String.valueOf(0);
+               return String.valueOf(number1 - number2);
+           }
+           else if (operator.equals("x")) {
+               if (String.valueOf(number1 * number2).endsWith(".0"))
+                   return String.valueOf((long)(number1 * number2));
 
-            if (String.valueOf(number1 / number2).endsWith(".0"))
-                return String.valueOf((long)(number1 / number2));
+               return String.valueOf(number1 * number2);
+           }
+           else if (operator.equals("/")) {
+               if (number2 == 0)
+                   return String.valueOf(0);
 
-            return String.valueOf( number1 / number2);
-        }
+               if (String.valueOf(number1 / number2).endsWith(".0"))
+                   return String.valueOf((long)(number1 / number2));
 
-        System.out.println("Unknown operator - " + operator);
+               return String.valueOf( number1 / number2);
+           }
+
+           System.out.println("Unknown operator - " + operator);
+       }
+       catch (Exception e) {
+           e.printStackTrace();
+       }
+
         return String.valueOf(0);
     }
 }
